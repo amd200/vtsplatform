313ab5e77bc68d1b7e24957616dfb7a2a0925812
Ahmed Mohamed <ahm12d.m@gmail.com>
2025-12-15 15:59:31 +0200

    remove about section

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
     <>
       <Hero />
       {/* <Features/> */}
-      <About/>
+      {/* <About/> */}
       <Instructors/>
       <Categories />
       <Books />
