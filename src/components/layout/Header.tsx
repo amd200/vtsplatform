d9eba2bc7e58e6f6bfccfe5ff10f56c3e371a08d
Ahmed Mohamed <ahm12d.m@gmail.com>
2025-12-15 16:35:38 +0200

    done

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -7,77 +7,77 @@ import { LogIn, Menu, UserPlus } from "lucide-react";
 import { getSession, signOut, useSession } from "next-auth/react";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
-import { ScrollProgress } from "@/components/ui/scroll-progress"
+import { ScrollProgress } from "@/components/ui/scroll-progress";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
 function Header() {
   const { data: session } = useSession();
   return (
-<>
-    <header className="sticky top-0 z-[100] bg-white">
-      <div className="container flex items-center justify-between py-3 font-ar-medium gap-x-5">
-        <div className="flex items-center gap-x-2">
-          <Menu className="max-lg:block hidden" />
-          <Link href="/">
-            <Image src={logo} alt={"logo"} />
-          </Link>
-        </div>
-        <ul className="lg:flex hidden gap-x-4 ms-5">
-          <li>
-            <Link href={"/"}>حول المنصة</Link>
-          </li>
-          <li>
-            <Link href={"/"}>المعرض</Link>
-          </li>
-          <li>
-            <Link href={"/"}>المحاضرين</Link>
-          </li>
-          <li>
-            <Link href={"/"}>تواصل معنا</Link>
-          </li>
-        </ul>
-        {!session?.user ? (
-          <div className="flex items-center gap-x-1">
-            {/* <Button >حجز السنتر</Button> */}
-            <Button>
-              <UserPlus />
-              <Link href={"/signup"}>جديد</Link>
-            </Button>
-            <Button>
-              <LogIn />
-              <Link href={"/login"}>دخول</Link>
-            </Button>
+    <>
+      <header className="sticky top-0 z-[100] bg-white">
+        <div className="container flex items-center justify-between py-3 font-ar-medium gap-x-5">
+          <div className="flex items-center gap-x-2">
+            <Menu className="max-lg:block hidden" />
+            <Link href="/" prefetch>
+              <Image src={logo} alt="logo" width={120} height={40} priority className="cursor-pointer" />
+            </Link>
           </div>
-        ) : (
-          <DropdownMenu>
-            <DropdownMenuTrigger>
-              <div className="flex items-center gap-x-3 flex-row-reverse">
-                <Image className="size-[35px] rounded-full" src={avatar} alt="" />
-                <div className="text-xs text-start">
-                  <span className="text-primary">مرحباً بك</span>
-                  <p>{session?.user?.StudentName?.split(" ").slice(0, 2).join(" ")}</p>
+          <ul className="lg:flex hidden gap-x-4 ms-5">
+            <li>
+              <Link href={"/"}>حول المنصة</Link>
+            </li>
+            <li>
+              <Link href={"/"}>المعرض</Link>
+            </li>
+            <li>
+              <Link href={"/"}>المحاضرين</Link>
+            </li>
+            <li>
+              <Link href={"/"}>تواصل معنا</Link>
+            </li>
+          </ul>
+          {!session?.user ? (
+            <div className="flex items-center gap-x-1">
+              {/* <Button >حجز السنتر</Button> */}
+              <Button>
+                <UserPlus />
+                <Link href={"/signup"}>جديد</Link>
+              </Button>
+              <Button>
+                <LogIn />
+                <Link href={"/login"}>دخول</Link>
+              </Button>
+            </div>
+          ) : (
+            <DropdownMenu>
+              <DropdownMenuTrigger>
+                <div className="flex items-center gap-x-3 flex-row-reverse">
+                  <Image className="size-[35px] rounded-full" src={avatar} alt="" />
+                  <div className="text-xs text-start">
+                    <span className="text-primary">مرحباً بك</span>
+                    <p>{session?.user?.StudentName?.split(" ").slice(0, 2).join(" ")}</p>
+                  </div>
                 </div>
-              </div>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent className="min-w-[9rem]" >
-              {/* <DropdownMenuLabel>My Account</DropdownMenuLabel> */}
-              {/* <DropdownMenuSeparator /> */}
-              <DropdownMenuItem asChild>
-                <Link href="/student" className="font-ar-medium flex-row-reverse text-sm">
-                  حسابي
-                </Link>
-              </DropdownMenuItem>{" "}
-              <DropdownMenuItem className="font-ar-medium flex-row-reverse text-sm" onClick={() => signOut()}>
-                تسجيل خروج
-              </DropdownMenuItem>
-              {/* <DropdownMenuItem>Team</DropdownMenuItem> */}
-              {/* <DropdownMenuItem>Subscription</DropdownMenuItem> */}
-            </DropdownMenuContent>
-          </DropdownMenu>
-        )}
-      </div>
-    </header>
-      <ScrollProgress  className="top-[73px]" />
-      </>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent className="min-w-[9rem]">
+                {/* <DropdownMenuLabel>My Account</DropdownMenuLabel> */}
+                {/* <DropdownMenuSeparator /> */}
+                <DropdownMenuItem asChild>
+                  <Link href="/student" className="font-ar-medium flex-row-reverse text-sm">
+                    حسابي
+                  </Link>
+                </DropdownMenuItem>{" "}
+                <DropdownMenuItem className="font-ar-medium flex-row-reverse text-sm" onClick={() => signOut()}>
+                  تسجيل خروج
+                </DropdownMenuItem>
+                {/* <DropdownMenuItem>Team</DropdownMenuItem> */}
+                {/* <DropdownMenuItem>Subscription</DropdownMenuItem> */}
+              </DropdownMenuContent>
+            </DropdownMenu>
+          )}
+        </div>
+      </header>
+      <ScrollProgress className="top-[73px]" />
+    </>
   );
 }
 
