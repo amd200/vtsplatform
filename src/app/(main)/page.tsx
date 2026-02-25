57342c48d42b2f8ff37ff234a68bfc79f77de9d0
Ahmed Mohamed <ahm12d.m@gmail.com>
2026-02-25 18:43:47 +0200

    done

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -65,6 +65,7 @@ export default async function Home() {
     <>
       <Hero />
       {/* <Instructors/> */}
+      
       <About />
       {/* {isLoggedIn && <Categories />} */}
       <Courses />
